2c2cf14ef1a6fbfa902e2a5b3f21a54dde6e8c3f
Rajat Malhotra <rajatmalhotra@gmail.com>
2013-07-11 17:14:24 +0530

    reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var fs = require('fs');
+var Buffer = require('buffer').Buffer;
+var constants = require('constants');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+/*
+	fs.readFile("index.html", function(err, data) {
+		if (err) {
+			console.log(err.message);
+			return;
+		}
+		console.log(data);
+		response.send(data);
+	});
+*/
+
+	var data = fs.readFileSync("index.html");
+	response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
